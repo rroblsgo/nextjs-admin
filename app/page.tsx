a22442b05311091d49ef91d39faf146c468632ff
Ricardo Robles <rroblesgo@gmail.com>
2024-10-04 22:23:18 +0200

    Añadidos Table y Search button code

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,6 @@ import {
   Menu,
   Package,
   Package2,
-  Search,
   ShoppingCart,
   Users,
 } from 'lucide-react';
@@ -29,9 +28,10 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
-import { Input } from '@/components/ui/input';
 import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
 import { ThemeToggle } from '@/components/theme-toggle';
+import Orders from '@/components/features/orders';
+import AdminSearch from '@/components/features/admin-search';
 
 export const description =
   'A products dashboard with a sidebar navigation and a main content area. The dashboard has a header with a search input and a user menu. The sidebar has a logo, navigation links, and a card with a call to action. The main content area shows an empty state with a call to action.';
@@ -46,11 +46,13 @@ export default function Dashboard() {
               <Package2 className="h-6 w-6" />
               <span className="">Acme Inc</span>
             </Link>
+
             <Button variant="outline" size="icon" className="ml-auto h-8 w-8">
               <Bell className="h-4 w-4" />
               <span className="sr-only">Toggle notifications</span>
             </Button>
           </div>
+
           <div className="flex-1">
             <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
               <Link
@@ -60,6 +62,7 @@ export default function Dashboard() {
                 <Home className="h-4 w-4" />
                 Dashboard
               </Link>
+
               <Link
                 href="#"
                 className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
@@ -70,6 +73,7 @@ export default function Dashboard() {
                   6
                 </Badge>
               </Link>
+
               <Link
                 href="#"
                 className="flex items-center gap-3 rounded-lg bg-muted px-3 py-2 text-primary transition-all hover:text-primary"
@@ -77,6 +81,7 @@ export default function Dashboard() {
                 <Package className="h-4 w-4" />
                 Products{' '}
               </Link>
+
               <Link
                 href="#"
                 className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
@@ -84,6 +89,7 @@ export default function Dashboard() {
                 <Users className="h-4 w-4" />
                 Customers
               </Link>
+
               <Link
                 href="#"
                 className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
@@ -93,6 +99,7 @@ export default function Dashboard() {
               </Link>
             </nav>
           </div>
+
           <div className="mt-auto p-4">
             <Card x-chunk="dashboard-02-chunk-0">
               <CardHeader className="p-2 pt-0 md:p-4">
@@ -111,6 +118,7 @@ export default function Dashboard() {
           </div>
         </div>
       </div>
+
       <div className="flex flex-col">
         <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
           <Sheet>
@@ -124,6 +132,7 @@ export default function Dashboard() {
                 <span className="sr-only">Toggle navigation menu</span>
               </Button>
             </SheetTrigger>
+
             <SheetContent side="left" className="flex flex-col">
               <nav className="grid gap-2 text-lg font-medium">
                 <Link
@@ -133,6 +142,7 @@ export default function Dashboard() {
                   <Package2 className="h-6 w-6" />
                   <span className="sr-only">Acme Inc</span>
                 </Link>
+
                 <Link
                   href="#"
                   className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
@@ -140,6 +150,7 @@ export default function Dashboard() {
                   <Home className="h-5 w-5" />
                   Dashboard
                 </Link>
+
                 <Link
                   href="#"
                   className="mx-[-0.65rem] flex items-center gap-4 rounded-xl bg-muted px-3 py-2 text-foreground hover:text-foreground"
@@ -150,6 +161,7 @@ export default function Dashboard() {
                     6
                   </Badge>
                 </Link>
+
                 <Link
                   href="#"
                   className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
@@ -157,6 +169,7 @@ export default function Dashboard() {
                   <Package className="h-5 w-5" />
                   Products
                 </Link>
+
                 <Link
                   href="#"
                   className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
@@ -164,6 +177,7 @@ export default function Dashboard() {
                   <Users className="h-5 w-5" />
                   Customers
                 </Link>
+
                 <Link
                   href="#"
                   className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
@@ -172,6 +186,7 @@ export default function Dashboard() {
                   Analytics
                 </Link>
               </nav>
+
               <div className="mt-auto">
                 <Card>
                   <CardHeader>
@@ -181,6 +196,7 @@ export default function Dashboard() {
                       support team.
                     </CardDescription>
                   </CardHeader>
+
                   <CardContent>
                     <Button size="sm" className="w-full">
                       Upgrade
@@ -190,19 +206,13 @@ export default function Dashboard() {
               </div>
             </SheetContent>
           </Sheet>
+
           <div className="w-full flex-1">
-            <form>
-              <div className="relative">
-                <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-                <Input
-                  type="search"
-                  placeholder="Search products..."
-                  className="w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-1/3"
-                />
-              </div>
-            </form>
+            <AdminSearch />
           </div>
+
           <ThemeToggle />
+
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
               <Button variant="secondary" size="icon" className="rounded-full">
@@ -210,6 +220,7 @@ export default function Dashboard() {
                 <span className="sr-only">Toggle user menu</span>
               </Button>
             </DropdownMenuTrigger>
+
             <DropdownMenuContent align="end">
               <DropdownMenuLabel>My Account</DropdownMenuLabel>
               <DropdownMenuSeparator />
@@ -220,24 +231,9 @@ export default function Dashboard() {
             </DropdownMenuContent>
           </DropdownMenu>
         </header>
+
         <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
-          <div className="flex items-center">
-            <h1 className="text-lg font-semibold md:text-2xl">Inventory</h1>
-          </div>
-          <div
-            className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm"
-            x-chunk="dashboard-02-chunk-1"
-          >
-            <div className="flex flex-col items-center gap-1 text-center">
-              <h3 className="text-2xl font-bold tracking-tight">
-                You have no products
-              </h3>
-              <p className="text-sm text-muted-foreground">
-                You can start selling as soon as you add a product.
-              </p>
-              <Button className="mt-4">Add Product</Button>
-            </div>
-          </div>
+          <Orders />
         </main>
       </div>
     </div>
